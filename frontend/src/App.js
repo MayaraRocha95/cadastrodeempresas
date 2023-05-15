985cb664beeb6fb981e7d1787a8b64246225b3cd
Mayara <mayaraaraujorocha@hotmail.com>
2023-05-15 17:50:28 -0300

    implementando toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import ListaEmpresas from "./components/ListaEmpresas";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <ListaEmpresas />
+      <ToastContainer />
     </div>
   );
 }
